906a668ce6874654b2a4326070b99d1f7abc4cbe
Vladimir Dimitrov <vkdimitrov@vjsoft.net>
2013-06-04 10:36:42 +0300

    init layout and canvas

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="layout.css">
+	</head>
+	<body>
+		<h1>Koch Snowflake</h1>
+		<canvas id="kochCanvas" width="780" height="780">
+		</canvas>
+	</body>
+</html>
